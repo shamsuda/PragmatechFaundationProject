c49fa0d616c0f0d627f0ca4c4b9d229f75bcce91
Samsuda <shamsuda.tinayev@gmail.com>
2022-04-07 18:03:44 +0400

    if,else,elif

---
+++ PythonStart/game.py
@@ -0,0 +1,5 @@
+#This is a comment
+print("Else və Elif arasında fərq nədir?")
+print("Elif: yada eger menasini verir. Eger verdiyimi if ve else Truee olmazsa onda bizim yada eger mansini veren Elif komutundan istifade edeciyik.")
+print("Mən İf Else istifadə edəndə niyə boşluq istifadə edirəm?")
+print("İf-den Else-den istifade edilerken bir setir asagi dusub sol terefden bosluq yazilmasam emelyat sistemi yazdigimi basa dusmur ve verdiyim emrleri icra etmir. Buda o dimek olurki eger men if-den ve ya else-den istifade ediremse abzasdan istifade etmeliyem.")
