1e07534258e91231bbd08f48fb6891f58298dd91
Sabuhi Gasimov <62444892+Sabuhi0@users.noreply.github.com>
2022-04-09 02:35:39 +0400

    Update tasks.py

--- PythonStart/tasks.py
+++ PythonStart/tasks.py
@@ -1,6 +1,7 @@
 #This is a comment
 print("Hello, World!")
 print("true+1 kodun neticesi: true sozu duzgun yailmayib T boyuk herflerle olmali idi.")
+print(true+1) # kodun neticesi: true sozu duzgun yailmayib T boyuk herf-de olmali idi.
 print("print(3>4 and 3+4/2) kodunn neticesi: False")
 print("print(type(int(float('3')))) kodun neticesi: <class 'int'>")
 print("3.14+int(3.14)) kodun neticesi: 6.140000000000001")
