5ff70cbb3f9789fec7271270b1353058d5a7131c
Samsuda <shamsuda.tinayev@gmail.com>
2022-04-13 23:42:22 +0400

    PythonFunctions

---
+++ PythonFunctions/task.py
@@ -0,0 +1,4 @@
+# a b-dən kiçikdirsə ekrana "Daxil edilən dəyərlər tələblərə uyğun deyil" çap edin
+a =  int(input("ededi qeyd edin:"))
+if a < 20 :
+    print("Daxil edilən dəyərlər tələblərə uyğun deyil") 
