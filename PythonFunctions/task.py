cdf44bd970a9372d2db08da0147f66e87431c6ea
Samsuda <shamsuda.tinayev@gmail.com>
2022-04-16 01:05:21 +0400

    bitdi

--- PythonFunctions/task.py
+++ PythonFunctions/task.py
@@ -1,4 +1,8 @@
-# Daxil edilən ədədin rəqəmlerinin cemini tap
+# a b-dən kiçikdirsə ekrana "Daxil edilən dəyərlər tələblərə uyğun deyil" çap edin
+a =  int(input("ededi qeyd edin:"))
+if a < 20 :
+    print("Daxil edilən dəyərlər tələblərə uyğun deyil") 
+    # Daxil edilən ədədin rəqəmlerinin cemini tap
 a=1
 def Artir():
     a==3
@@ -6,10 +10,23 @@ def Artir():
     print(a)
     Artir()
     
-# Daxil edilən ədədin rəqəmlerini ayrı ayrı çap edin
-def Artir():
-    global a
-    print(a)
-    if a<100:
-        a+=1
-        Artir()
+# a b-dən böyükdürsə a və b ədədləri arasında 5-ə bölünən amma 7-yə bölünməyən ədədləri ekrana çap edin.(a=50,b=20)
+b=20 
+print(b)
+b=b+5
+print(b)
+b=b+10
+print(b)
+b=b+15
+print(b)
+b=b+25
+
+# a və b arasında olan ədədlərin cəmini return edin.1 den-100 e qeder ededlerin cemini cap et
+def ededleriTopla():
+  i=0
+  cemi=0
+  while i<100:
+    cem=cem+1
+    i=i+1
+    print(cem)
+    ededleriTopla()
