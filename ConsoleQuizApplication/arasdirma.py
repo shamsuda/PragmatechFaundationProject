4bae5d88e80855ddc8630c1c93e029c9f108b802
Samsuda <shamsuda.tinayev@gmail.com>
2022-04-25 23:40:10 +0400

    arasdirmada 1 sual qaldi

---
+++ ConsoleQuizApplication/arasdirma.py
@@ -0,0 +1,3 @@
+print("Python module nədir?")
+print("Bezi isleri rahat yerine yetirilmesi ucun istifade olunan bir mexanizm kimi baxa bilerik.")
+#-------------------------------------------------------------------------------------------------------
