231d1c48687615513931aaaf5c38cf7c551cb846
Samsuda <shamsuda.tinayev@gmail.com>
2022-04-27 00:13:37 +0400

    quiz day1

--- ConsoleQuizApplication/quiz.py
+++ ConsoleQuizApplication/quiz.py
@@ -2,11 +2,11 @@ from ast import If
 
 
 sual="""
- 1. 4+5=?
-  A)6
-  B)7
-  C)8
-  D)9
+ 1. Dunyanin en uzun çayı hansidir?
+  A)Lena çayı
+  B) Mississippi çayı
+  C) Nil çayı
+  D) Amazon çayı
  """
 
 print(sual)
