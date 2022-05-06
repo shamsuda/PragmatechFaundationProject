69018fe3bb5e090d0d8b4d4819db81a506e87c3d
Samsuda <shamsuda.tinayev@gmail.com>
2022-05-06 22:49:33 +0400

    isin ortasi

---
+++ ConsoleQuizApplication/tapsiriq.py
@@ -0,0 +1,10 @@
+a="""[
+    {"Restoran":"coffe restoran",
+    "Gelecek plan":"Restoranin yeni filyallarini acmaq",
+    "Restoranin Yaranma Tarixi":"01.01.2022",
+    "Sahibi":"Mikayil Mikayilov",
+    "Restorana devet buyuran musterilerin fikirleri":"Yaxsi",}
+    ]
+"""
+
+print (a[{"restoran"}])
