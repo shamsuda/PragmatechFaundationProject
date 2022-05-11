6d192deaf13ed29df07f7ca7f65cd29f916c4f38
Samsuda <shamsuda.tinayev@gmail.com>
2022-05-11 23:35:17 +0400

    python arasdirma

---
+++ Arasdirma Python/arasdirma.py
@@ -0,0 +1,32 @@
+###QWidget
+##Bu proqramlaşdırma dili deyil. Bu, bizə Python ilə qrafik istifadəçi interfeysləri ilə proqramlar yaratmağa imkan verir.
+
+###QTimeEdit
+##QTimeEdit sinfi QDateTimeEdit vidcetinə əsaslanaraq vaxtları redaktə etmək üçün vidjet təqdim edir .
+
+###QTabWidget
+##QTabWidget sinfi sekmeli vidjetlər yığınını təmin edir.
+
+###QPushButton
+##QPushButton vidceti əmr düyməsini təmin edir, tek emir deyilde tekan verme butonlarini yaratmaqda komek edir.
+
+###QMenu
+##QMenu sinfi menyu panellərində, kontekst menyularında və digər açılan menyularda istifadə üçün menyu vidceti təqdim edir. 
+
+###QLabel
+##QLabel vidceti mətn və ya şəkil ekranını təmin edir.
+
+###QComboBox
+##QComboBox vidceti birləşdirilmiş düymə və açılan siyahıdır. 
+
+###QScrollArea
+##QScrollArea sinfi başqa vidcetə sürüşmə görünüşü təqdim edir.
+
+###QtCore
+##Qt Core modulu bu xüsusiyyətləri C++-a əlavə edir.
+
+###QtWidgets
+##QWidget sinfi bütün istifadəçi interfeysi obyektlərinin əsas sinfidir.
+
+###QtGui
+##QtGui modulu QtCore - u GUI funksionallığı ilə genişləndirir.
