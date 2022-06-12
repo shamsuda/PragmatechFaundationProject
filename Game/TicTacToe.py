5487d8ae1d553d89d75dbd67e0c7cc134dd21f0d
Samsuda <shamsuda.tinayev@gmail.com>
2022-06-12 20:55:53 +0400

    tictacteogame

--- Game/TicTacToe.py
+++ Game/TicTacToe.py
@@ -1,13 +1,22 @@
+from calendar import setfirstweekday
 from curses import BUTTON_SHIFT
+from re import X
+from tkinter.tix import Tree
+from typing_extensions import Self
 from PySide6.QtWidgets import *
 from PySide6.QtCore import *
 from PySide6.QtGui import *
+sutunSayi=3
+setirSayi=3
 w=600
-btnW=w/3
+btnW=sutunSayi
 h=600
-btnH=h/3
+btnH=h/setirSayi
 btnX=0
 btnY=0
+
+btnStatus=True
+
 
 def clikcFunc():
     print('x')
@@ -16,12 +25,32 @@ app=QApplication()
 
 game=QWidget()
 game.resize(w,h)
-for setir in range(1,4):
- for sutun in range(1,4):
-     btn=QPushButton('{setir}.{sutun}', game)
+class TicButton(QPushButton):
+    def __init__(self,*args):
+      QPushButton.__init__(self,*args)
+      self.clicked.connect(self.clikcFunc)
+      self.setStyleSheet("background:lightgray; font-size:25px")
+      self.clickCount=0
+      
+    def clikcFunc(self):
+     global btnStatus
+     self.clickCount+=1
+     if self.clickCount<=1:
+      if btnStatus:
+         self.setText('X')
+         btnStatus=False
+      else:
+        self.setText('O')
+        btnStatus=True
+        
+    print(self.clickCount)
+       
+for setir in range(1,setirSayi+1):
+ for sutun in range(1,sutunSayi+1):
+     btn=TicButton('', game)
      btn.setGeometri(btnX,btnY, btnW,btnH)
-     btn.setStyleSheet("background:lightgray")
-     btn.clicked.connect(clikcFunc)
+     btn.setStyleSheet("background:lightgray;")
+    
      btnX+=btnW
      btnX=0
      btnY+=btnH
